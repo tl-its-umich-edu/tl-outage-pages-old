d9653f218987c3c512d53d91c5caa4f7d36183c1
David Haines <dlhaines@umich.edu>
2017-04-04 15:48:16 -0400

    Tlpso 397 - Static T&L Outage pages. (#1)
    * modified static dir name.
    
    * removed cosign-specific cert hashing.
    
    * clean up.
    
    * Checkpoint changes to run on local Docker
    
    * TLPSO-397: add more outage pages, move outage pages to outage subdirectory
    
    * TLPSO-397: add httpd.conf
    
    * TLPSO-397: setup to run locally as well as in OpenShift
    
    * TLPSO-397: update for running local docker, only target outage pages
    
    * TLPPSO-397: cleanup

---
+++ run_local_docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# run_local_docker will set up local environment so that we can run the same
+# Dockerfile locally and on OpenShift.  Resources required locally are
+# put in the external directory.
+
+#set -x
+# Make a common image tag for the build and the run.
+TAG=outage_a
+
+# Use this local file copy instead of the OpenShift secret so can run
+# on localhost 8080.  This file is customized to run on a local docker
+# instance.
+
+# if no httpd.conf file exists create one.
+if [[ ! -e $(pwd)/external/httpd.conf ]]; then
+    cp $(pwd)/external/httpd.conf.local.8080 $(pwd)/external/httpd.conf
+fi
+
+# if the provided one is newer than the local httpd.conf copy over the local one.
+ if [[ $(pwd)/external/httpd.conf.local.8080 -nt $(pwd)/external/httpd.conf ]]; then
+     cp $(pwd)/external/httpd.conf.local.8080 $(pwd)/external/httpd.conf
+ fi
+ 
+# Build the image, mount the local file, and set up the ports to expose.
+# (Expose in the docker file is a suggestion and is overridden here.)
+docker build -t ${TAG} . \
+    && docker run -v $(pwd)/external:/tmp/apache-conf -p 8080:8080 ${TAG}
+
+#end
